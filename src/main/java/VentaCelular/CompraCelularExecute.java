aebd0f7d76e72c4f612eda25ce0d9073ce45d27d
kelvinOjeda071 <kelvin.ojeda.2020b@gmail.com>
2022-06-22 00:53:45 -0500

    Se resuelven los problemas asociados al posicionamiento relativo dentro de los archivos de información dentro del proyecto, para que de esta manera Jenkins pueda conocer la ruta relativa respecto a la vista del servidor de pruebas.

--- src/main/java/VentaCelular/CompraCelularExecute.java
+++ src/main/java/VentaCelular/CompraCelularExecute.java
@@ -4,9 +4,9 @@ public class CompraCelularExecute {
     public static void main(String[] args) {
         Empresa e = new Empresa("Empresa de prueba");
         /*Se cargan los datos asociados a los telefonos con los que se encuentra a disposición*/
-        e.cargarCelulares("\\data\\celulares.txt");
+        e.cargarCelulares("..//data//celulares.txt");
         /*Se cargan las cuentas ingresadas con anterioridad*/
-        e.cargarCuentas("\\data\\cuentaClientes.txt");
+        e.cargarCuentas("..//data//cuentaClientes.txt");
         /*Se muestran los celulares disponibles dentro del stock*/
         e.mostrarCelulares();
         /*Se cambia el estado del teléfono a no disponible*/
